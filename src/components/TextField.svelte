3fd941ddc2b0a284a641fd890bacf6a61146c110
code@franciswibisono.com <code@franciswibisono.com>
2022-10-16 01:34:54 +0700

    responsive

--- src/components/TextField.svelte
+++ src/components/TextField.svelte
@@ -2,12 +2,12 @@
 	import { userStore } from '../lib/stores';
 	export let componentName: string;
 	export let componentValue: string|number;
-	export let type: 'short' | 'medium' | 'long' = 'medium'
+	export let type: 'short' | 'medium' | 'long' | 'full' = 'medium'
 
 
 </script>
 
-<div class="flex flex-col {type==='long' && 'w-64'} {type==='medium' && 'w-32'} {type==='short' && 'w-16'}">
+<div class="flex flex-col {type==='long' && 'w-64'} {type==='medium' && 'w-32'} {type==='short' && 'w-16'} {type==="full" && 'w-full'}">
 	<label for={componentName}> {componentName} </label>
-	<input id="{componentName}" type='text' bind:value={componentValue} />
+	<input id="{componentName}" type='text' bind:value={componentValue} class="p-2" />
 </div>
